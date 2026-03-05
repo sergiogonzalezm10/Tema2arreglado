8a3cf6839808d1bae10a2682451796d3e72e1f17
sergiogonzalezm10 <sergio.gonzalezm@iesnervion.es>
2026-03-05 14:16:29 +0100

    tdt

---
+++ src/ejerciciosexamen/Ejerciciofinal.java
@@ -0,0 +1,83 @@
+package ejerciciosexamen;
+
+import java.util.Scanner;
+
+public class Ejerciciofinal {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+        System.out.print("Introduzca el tamaño del lado: ");
+        int n = sc.nextInt();
+
+        int filas = 2 * n - 1;
+
+        for (int i = 1; i <= filas; i++) {
+            int numAsteriscos;
+            int espaciosInicio;
+
+            if (i <= n) {
+                // Primera mitad: líneas de tamaño creciente
+                numAsteriscos = n + 2 * (i - 1);
+                espaciosInicio = n - i;
+            } else {
+                // Segunda mitad: líneas de tamaño decreciente
+                int k = i - n;
+                numAsteriscos = n + 2 * (filas - i);
+                espaciosInicio = k;
+            }
+
+            // Espacios iniciales para centrar la figura
+            for (int j = 0; j < espaciosInicio; j++) {
+                System.out.print(" ");
+            }
+
+            // Asteriscos contiguos
+            for (int j = 0; j < numAsteriscos; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Introduce el tamaño del triángulo: ");
+int lado = sc.nextInt();
+
+for (int i = 0; i < lado; i++) {
+    // Imprimir espacios a la izquierda para desplazar el triángulo
+    for (int espacio = 0; espacio < i; espacio++) {
+        System.out.print(" ");
+    }
+
+    if (i == 0) {
+        // Primera fila: imprimir * separados por espacio
+        for (int j = 0; j < lado; j++) {
+            System.out.print("*");
+            if (j < lado - 1) System.out.print(" ");
+        }
+    } else if (i == lado - 1) {
+        // Última fila: solo un *
+        System.out.print("*");
+    } else {
+        // Filas intermedias: un * al inicio y otro al final
+        System.out.print("*");
+        // espacios entre los dos asteriscos
+        int espaciosInternos = 2 * (lado - i) - 3;
+        for (int s = 0; s < espaciosInternos; s++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+    }
+
+    System.out.println();
+}
+
+sc.close();
+}
+}
